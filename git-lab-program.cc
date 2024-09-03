80f4447b5c20d84a77092f8a2832c6a221015b54
Joey Thoma <jthoma2005@gmail.com>
2024-09-03 18:11:01 -0400

    yippee

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joey Thoma
+ *        Date: 09/03/2024
+ * Description: Simple "hello git" program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
